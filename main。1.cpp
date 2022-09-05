dc2d35b4602caf98d387076305147d17065d17ba
wzy <110782639+wzylro@users.noreply.github.com>
2022-09-05 22:03:06 +0800

    Add files via upload

---
+++ main。1.cpp
@@ -0,0 +1,93 @@
+//#include <iostream>
+//using namespace std;
+//int main (int argc,char**argv)
+//{
+//cout<<"This is my 1st c++ programme"<<endl; 
+//return 0;
+//}
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int a,b,c,d,e,f,g;
+//    cin>>a>>b>>c>>d>>e>>f>>g; 
+//    int ALL=a+b+c+d+f+e+g;
+//    cout<<ALL<<endl;
+//    return 0;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{  double a=0.1,b=0.2;
+//   double c=a+b;
+//   cout<<c<<endl;
+//   cout<<a*b/c<<endl; 
+//   return 0; 
+//}
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
